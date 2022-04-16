100e863f25284a0bb68ed991c2dc53227a7db0dc
bayupw <bayupw@gmail.com>
2022-04-17 07:18:46 +1200

    update output

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "instance_web_private_ip" {
   value       = module.web_server.aws_instance.private_ip
 }
 
-output "private_link_dns" {
+output "privatelink_dns_name" {
   description = "PrivateLink DNS Name"
   value       = aws_vpc_endpoint.web_ep.dns_entry[0].dns_name
 }
 
-output "private_private_ip" {
+output "privatelink_private_ip" {
   description = "PrivateLink Private IP"
   value       = data.aws_network_interface.web_ep_eni.private_ip
 }
