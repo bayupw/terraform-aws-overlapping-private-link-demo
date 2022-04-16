1dcfee4938282dc5216b25c9fa6c2a6203fe6b9d
bayupw <bayupw@gmail.com>
2022-04-17 07:14:54 +1200

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
-output "instance_client" {
-  description = "Client"
-  value       = "${module.client.aws_instance.id} | Private IP: ${module.client.aws_instance.private_ip}"
+output "instance_client_private_ip" {
+  description = "Client Private IP"
+  value       = module.client.aws_instance.private_ip
 }
 
-output "instance_web" {
-  description = "Web Server"
-  value       = "${module.web_server.aws_instance.id} | Private IP: ${module.web_server.aws_instance.private_ip}"
+output "instance_web_private_ip" {
+  description = "Web Server Private IP"
+  value       = module.web_server.aws_instance.private_ip
 }
 
-output "privatelink" {
-  description = "PrivateLink / VPC Endpoint"
-  value       = "${aws_vpc_endpoint.web_ep.id} | DNS name: ${aws_vpc_endpoint.web_ep.dns_entry[0].dns_name} | Private IP: ${data.aws_network_interface.web_ep_eni.private_ip}"
+output "private_link_dns" {
+  description = "PrivateLink DNS Name"
+  value       = aws_vpc_endpoint.web_ep.dns_entry[0].dns_name
 }
 
-output "nlb" {
-  description = "NLB"
-  value       = "${module.nlb.lb_id} | Private IP: ${data.aws_network_interface.nlb_eni.private_ip}"
+output "private_private_ip" {
+  description = "PrivateLink Private IP"
+  value       = data.aws_network_interface.web_ep_eni.private_ip
+}
+
+output "nlb_private_ip" {
+  description = "NLB Private IP"
+  value       = data.aws_network_interface.nlb_eni.private_ip
 }
