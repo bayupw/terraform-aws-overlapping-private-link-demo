ef53d5bf3216cd865e0b9c6f6b4de8084299e4a6
bayupw <bayupw@gmail.com>
2022-04-17 06:33:26 +1200

    update default var

--- variables.tf
+++ variables.tf
@@ -1,4 +1,18 @@
 variable "vpcs" {
   description = "Maps of VPCs' attributes"
   type        = map(any)
+  default = {
+    vpc_a = {
+      name           = "PrivateLink-VPC-A"
+      cidr           = "10.0.0.0/16"
+      azs            = ["ap-southeast-2a"]
+      public_subnets = ["10.0.0.0/24"]
+    }
+    vpc_b = {
+      name           = "PrivateLink-VPC-B"
+      cidr           = "10.0.0.0/16"
+      azs            = ["ap-southeast-2a"]
+      public_subnets = ["10.0.0.0/24"]
+    }
+  }
 }
