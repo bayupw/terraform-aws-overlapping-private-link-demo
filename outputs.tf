c0cd5fa2e3b70964d31f87628d4f2ac7d7d9ff94
bayupw <bayupw@gmail.com>
2022-05-02 17:35:03 +1200

    update output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,19 @@
-output "instance_client_private_ip" {
+output "client_ssm_session" {
+  description = "Client Instance SSM command"
+  value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.client.aws_instance.id}"
+}
+
+output "client_private_ip" {
   description = "Client Private IP"
   value       = module.client.aws_instance.private_ip
 }
 
-output "instance_web_private_ip" {
+output "web_ssm_session" {
+  description = "Client Instance SSM command"
+  value       = "aws ssm start-session --region ${data.aws_region.current.name} --target ${module.web_server.aws_instance.id}"
+}
+
+output "web_private_ip" {
   description = "Web Server Private IP"
   value       = module.web_server.aws_instance.private_ip
 }
